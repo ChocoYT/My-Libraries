02c6d54549594b0442e36bd6294859c83d003917
Ethan Tucker <ethan.tucker8767@gmail.com>
2024-06-14 19:15:36 +0100

    Finished matrix class, added more functions

--- My_Library/math.py
+++ My_Library/math.py
@@ -1,9 +1,32 @@
+# Angle conversions
+from math import radians as rad
+from math import degrees as deg
+
+# Trigonometric functions
+from math import sin, cos, tan
+from math import asin, acos, atan
+from math import sinh, cosh, tanh
+from math import asinh, acosh, atanh
+
 # Irrational and repeating numbers
 pi = 3.1415926535897932
 e = 2.7182818284590452
 phi = 1.6180339887498948
 
 third = 1 / 3
+
+# Rounding
+def floor(n: int | float) -> int:
+    if round(n) > n:
+        n -= 1
+    
+    return round(n)
+
+def ceil(n: int | float) -> int:
+    if round(n) < n:
+        n += 1
+
+    return round(n)
 
 # Roots
 def sqrt(n: int | float) -> float:
@@ -29,28 +52,41 @@ class Vector2():
 
 # Matrix
 class Matrix():
-    def __init__(self, width: int | None, height: int | None, *rows: list | tuple) -> None:
-        self.matrix = list(rows)
+    def __init__(self, matrix: list | tuple, width: int = None, height: int = None, placeholder: int = 0) -> None:
+        self.matrix = list(matrix)
 
-        if self.getWidth() < width:
-            for row in self.matrix:
-                if len(row) < width:
-                    row.extend([0 for _ in range(len(row) - height)])
+        # Makes the matrix the correct width
+        if width != None:
+            if self.getWidth() < width:
+                for i, row in enumerate(self.matrix):
+                    if len(row) < width:
+                        self.matrix[i].extend([placeholder for _ in range(len(row) - width)])
+
+        # Makes the matrix the correct height
+        if height != None:
+            matrixHeight = self.getHeight()
+            if matrixHeight < height:
+                for _ in range(height - matrixHeight):
+                    self.matrix.append([placeholder for _ in range(width)])
 
     def mult(self, matrix):
-        if self.getWidth() == matrix.getHeight():
-            result = Matrix()
+        if self.getHeight() == matrix.getWidth():
 
-            for row in matrix.matrix:
-                for row in self.matrix:
-                    pass
+            result = [
+                    [
+                        sum(a * b for a, b in zip(A_row, B_col))
+                        for B_col in zip(*matrix.matrix)
+                    ]   for A_row in self.matrix
+                ]
         else:
-            print("Failed to multiply matrix")
-            exit()
+            raise Exception("Failed to multiply matrix")
+
+        return Matrix(result)
 
     def getWidth(self) -> int:
         width = 0
-        for rowWidth in self.matrix:
+        for row in self.matrix:
+            rowWidth = len(row)
             if rowWidth > width:
                 width = rowWidth
 
--- shell.py
+++
@@ -1,3 +0,0 @@
-from My_Library import math
-
-math.Matrix([])
